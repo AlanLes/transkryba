ecb90a5d63e1ba079aa01351fb2277229832bc68
AlanLes <a.leszczuk94@gmail.com>
2026-04-03 15:19:13 +0200

    refactor: decompose monolithic page into modular Next.js App Router architecture

---
+++ app/_components/transcription-widget.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useTranscription } from "../_hooks/use-transcription";
+import FileUploadSection from "./file-upload-section";
+import TranscriptionResult from "./transcription-result";
+import ErrorAlert from "./error-alert";
+
+export default function TranscriptionWidget() {
+  const {
+    file,
+    isLoading,
+    transcription,
+    error,
+    fileInputRef,
+    handleFileChange,
+    handleTranscribe,
+    handleReset,
+  } = useTranscription();
+
+  return (
+    <>
+      <FileUploadSection
+        file={file}
+        isLoading={isLoading}
+        hasTranscription={!!transcription}
+        fileInputRef={fileInputRef}
+        onFileChange={handleFileChange}
+        onTranscribe={handleTranscribe}
+        onReset={handleReset}
+      />
+      {error && <ErrorAlert message={error} />}
+      {transcription && <TranscriptionResult text={transcription} />}
+    </>
+  );
+}
